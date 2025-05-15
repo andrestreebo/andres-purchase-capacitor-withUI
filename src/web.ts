e834c830b87b1cf6d6af7a9309af87bc6f3d7dd3
Andres Solleiro <andressolleiro@MacBook-Pro-de-Andres.local>
2025-05-16 00:34:58 +0200

    Fix unused parameter warnings

--- src/web.ts
+++ src/web.ts
@@ -3,14 +3,14 @@ import { WebPlugin } from '@capacitor/core';
 import type { PaywallOptions, PaywallResult, PurchasesUIPlugin } from './definitions';
 
 export class PurchasesUIWeb extends WebPlugin implements PurchasesUIPlugin {
-  async presentPaywall(options: PaywallOptions): Promise<PaywallResult> {
+  async presentPaywall(_options: PaywallOptions): Promise<PaywallResult> {
     console.warn('PurchasesUI is not implemented on web. Purchase result will simulate a declined purchase.');
     return {
       didPurchase: false,
     };
   }
 
-  async presentFooterPaywall(options: PaywallOptions): Promise<PaywallResult> {
+  async presentFooterPaywall(_options: PaywallOptions): Promise<PaywallResult> {
     console.warn('PurchasesUI is not implemented on web. Purchase result will simulate a declined purchase.');
     return {
       didPurchase: false,
