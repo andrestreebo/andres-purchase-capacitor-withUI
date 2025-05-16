aae3940297569482a92846acc1dba04995f84ad0
Andres Solleiro <andressolleiro@MacBook-Pro-de-Andres.local>
2025-05-16 10:22:42 +0200

    Fix RevenueCatUI imports and dependencies

--- ios/Sources/ASBPurchasesUIPlugin/ASBPurchasesUI.swift
+++ ios/Sources/ASBPurchasesUIPlugin/ASBPurchasesUI.swift
@@ -1,13 +1,14 @@
 import Foundation
 import RevenueCat
+import RevenueCatUI
 
 @objc public class ASBPurchasesUI: NSObject {
     @objc public func configure(apiKey: String) {
         Purchases.configure(withAPIKey: apiKey)
     }
     
-    @objc public func presentPaywall(offeringId: String, viewController: UIViewController, completion: @escaping (PaywallResult) -> Void) {
-        PaywallViewController.presentPaywall(
+    @objc public func presentPaywall(offeringId: String, viewController: UIViewController, completion: @escaping (RevenueCatUI.PaywallResult) -> Void) {
+        RevenueCatUI.PaywallViewController.presentPaywall(
             forOfferingID: offeringId,
             on: viewController, 
             animated: true,
--- ios/Sources/ASBPurchasesUIPlugin/ASBPurchasesUIPlugin.swift
+++ ios/Sources/ASBPurchasesUIPlugin/ASBPurchasesUIPlugin.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Capacitor
 import RevenueCat
+import RevenueCatUI
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
